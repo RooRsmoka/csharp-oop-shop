120f00abe7c317febd59568b57d5a4b550b806d2
RooRsmoka <chri.disa96@gmail.com>
2022-05-13 18:08:54 +0200

    Aggiungere i file di progetto.

---
+++ Prodotto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Prodotto
+    {
+        public int Codice { get; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public double Prezzo { get; set; }
+
+        private double iva;
+        public double Iva 
+        {
+            get { return iva; }
+            set { iva = value; } 
+        }
+
+        private Random myRandom = new Random();
+
+
+        public Prodotto(string nome, string descrizione, double prezzo, double iva)
+        {
+            Nome = nome;
+            Descrizione = descrizione;
+            Prezzo = prezzo;
+            Iva = iva;
+            Codice = myRandom.Next();
+        }
+
+        public double GetPrezzoNonIvato()
+        {
+            return Prezzo;
+        }
+        public double GetPrezzoIvato()
+        {
+            return Prezzo * (1.0 + Iva);
+        }
+        public string GetNomeProdotto()
+        {
+            return String.Format("Nome esteso: {0}_{1}", Codice, Nome);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using csharp_oop_shop;
+// See https://aka.ms/new-console-template for more information
+
+
+//Console.WriteLine("Hello, World!");
+//Random myRandom = new Random();
+//Console.WriteLine(myRandom.Next());
+
+
+Prodotto prodottoOne = new Prodotto("Tastiera","Tastiera meccanica da gaming", 89.99, 0.22);
+
+Console.WriteLine("Prodotto N°1: Nome: {0}, Descrizione: {1}, Codice: {2}, {3}, Prezzo di partenza: {4}, Prezzo con IVA: {5}", 
+    prodottoOne.Nome, prodottoOne.Descrizione, prodottoOne.Codice, prodottoOne.GetNomeProdotto(), prodottoOne.GetPrezzoNonIvato(), prodottoOne.GetPrezzoIvato()); 
